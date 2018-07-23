14feaba36dee54b291f2dcb608b1e94c86cb28b1
Barry Hicks <Barry.Hicks@sas.com>
2018-07-23 15:47:31 -0400

    initial commit

---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/brob06dur/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('brob06dur/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('brob06dur')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
