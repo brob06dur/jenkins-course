783a288694ffec95808abd2d567a194bdbaf8aee
Barry Hicks <Barry.Hicks@sas.com>
2018-07-24 14:33:17 -0400

    remove datetimestamp from nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -15,7 +15,7 @@ job('NodeJS Docker example') {
     steps {
         dockerBuildAndPublish {
             repositoryName('brob06dur/docker-nodejs-demo')
-            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}')
+            tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
